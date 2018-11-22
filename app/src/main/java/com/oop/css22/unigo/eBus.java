b48fe23ad8efa974dc3e402fe9a64d23212a5047
orbitalno11 <orbitalno11@users.noreply.github.com>
2018-11-22 18:13:24 +0700

    insert e bus

---
+++ app/src/main/java/com/oop/css22/unigo/eBus.java
@@ -0,0 +1,42 @@
+package com.oop.css22.unigo;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class eBus extends Fragment {
+
+
+    public eBus() {
+        // Required empty public constructor
+    }
+
+    public static eBus newInstrance(){
+        return new eBus();
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_e_bus, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("เส้นทาง e-bus");
+
+
+    }
+}
